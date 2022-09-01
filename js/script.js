fd4f3be1a11c1c444c6d6ba863a4cd9318190ced
siham <ma.broukysiham@gmail.com>
2022-09-01 22:05:06 +0200

    updete button add to card

--- js/script.js
+++ js/script.js
@@ -22,19 +22,19 @@ let addtocard = document.querySelector('.addtocard');
 //total price 
  
 
-addtocard.onclick = function(){
+// addtocard.onclick = function(){
  
-  if(nb !==0){
-    ntf = nb;
-    notification.innerHTML = ntf;
+//   if(nb !==0){
+//     ntf = nb;
+//     notification.innerHTML = ntf;
  
-    notification.style.display='block';  
-  }
+//     notification.style.display='block';  
+//   }
  
  
  
 
-}
+// }
 
 // console.log(ntf);
 // if(ntf!==0){
@@ -56,10 +56,152 @@ mins.onclick = function(){
 
 
 
-btn.onclick = function()
+// btn.onclick = function()
+// {
+//     ///empty
+//     if(ntf === 0){
+        
+  
+//         cartcontainer.style.display='block'
+//         let cart1 = document.createElement("span");
+//         let cart1text = document.createTextNode('cart');
+//         cart1.className='cart1';
+//         cart1.appendChild(cart1text);
+
+//         let pempty = document.createElement("p");
+//         let pemptytext = document.createTextNode('your cart empty');
+//         pempty.className='empty';
+//         pempty.appendChild(pemptytext);
+//         pempty.style.display='block'
+
+//         cartcontainer.appendChild(cart1);
+//         cartcontainer.appendChild(pempty);
+       
+
+//     }else{
+
+//         cartcontainer.style.display='block'
+//         console.log('hi dev siham');
+
+//         let cart1 = document.createElement("span");
+//         let cart1text = document.createTextNode('cart');
+//         cart1.className='cart1';
+//         cart1.appendChild(cart1text);
+//             //notempty
+//                 let notempty = document.createElement("div");
+//                 notempty.className='notempty';
+//                 //imagele
+//                 let imag = document.createElement("div");
+//                 let imspro = document.createElement("img");
+//                 imspro.src='images/image-product-1.jpg';
+//                 imag.appendChild(imspro);
+//                 //pric ele
+//                 let yourpric = document.createElement("div"); 
+//                 yourpric.className=('yourpric')
+//                 let p1 = document.createElement("p");
+//                 let p1text =document.createTextNode('aaaaaaaaaaaaa');
+//                 p1.appendChild(p1text);
+    
+//                 let p2 = document.createElement("p");
+//                 let p2text =document.createTextNode(`${nbPrice} x ${ntf}`);
+//                 p2.appendChild(p2text);  
+    
+//                 yourpric.appendChild(p1);
+//                 yourpric.appendChild(p2);
+//                     //suupp ele
+//                     let supp= document.createElement("div");
+//                     let icondelet =document.createElement('i');
+//                     icondelet.classList='fa fa-trash';
+//                 supp.appendChild(icondelet);
+        
+//             notempty.appendChild(imag);
+//             notempty.appendChild(yourpric);
+//             notempty.appendChild(supp);
+    
+//             //buttun checkout
+//             let butcheckout =document.createElement('button');
+//             let  butcheckouttext =document.createTextNode('checkout');
+//             butcheckout.appendChild(butcheckouttext);
+//             butcheckout.className=("checkout");
+//             notempty.appendChild(butcheckout);
+    
+//         cartcontainer.appendChild(cart1);
+//         cartcontainer.appendChild(notempty);
+//     }
+
+   
+// };
+// lin lsit
+let menu = document.querySelector('.menu');
+let menu1 = document.querySelector('.menu1');
+let listlink = document.querySelector('.listlink');
+menu.onclick= function(){
+   menu.style.display='block'
+    listlink.style.display='block'
+   menu1.style.display='block'  
+
+};
+menu1.onclick= function(){
+    menu.style.display='block'
+     listlink.style.display='none'
+    menu1.style.display='none'  
+ 
+ };
+//  change src img 
+let mainimg = document.querySelector('.mainimg');
+let img1 = document.querySelector('.img1');
+let img2 = document.querySelector('.img2');
+let img3 = document.querySelector('.img3');
+let img4 = document.querySelector('.img4');
+img1.onclick = function(){
+    mainimg.src ='images/image-product-1.jpg';
+}
+img2.onclick = function(){
+    mainimg.src ='images/image-product-2.jpg';
+}
+img3.onclick = function(){
+    mainimg.src ='images/image-product-3.jpg';
+}
+img4.onclick = function(){
+    mainimg.src ='images/image-product-4.jpg';
+}
+// slide for version mobile
+
+
+
+
+
+
+
+
+// addtocard.onclick = function(){
+ 
+//     if(nb !==0){
+//       ntf = nb;
+//       notification.innerHTML = ntf;
+   
+//       notification.style.display='block';  
+//     }
+   
+   
+   
+  
+//   }
+
+
+  addtocard.onclick = function()
 {
+
+    if(nb !==0){
+        ntf = nb;
+        notification.innerHTML = ntf;
+     
+        notification.style.display='block';  
+      }
+     
     ///empty
     if(ntf === 0){
+        
   
         cartcontainer.style.display='block'
         let cart1 = document.createElement("span");
@@ -92,7 +234,7 @@ btn.onclick = function()
                 //imagele
                 let imag = document.createElement("div");
                 let imspro = document.createElement("img");
-                imspro.src='images/image-product-1.jpg';
+                imspro.src= mainimg;
                 imag.appendChild(imspro);
                 //pric ele
                 let yourpric = document.createElement("div"); 
@@ -130,41 +272,17 @@ btn.onclick = function()
 
    
 }
-// lin lsit
-let menu = document.querySelector('.menu');
-let menu1 = document.querySelector('.menu1');
-let listlink = document.querySelector('.listlink');
-menu.onclick= function(){
-   menu.style.display='block'
-    listlink.style.display='block'
-   menu1.style.display='block'  
 
-};
-menu1.onclick= function(){
-    menu.style.display='block'
-     listlink.style.display='none'
-    menu1.style.display='none'  
- 
- };
-//  change src img 
-let mainimg = document.querySelector('.mainimg');
-let img1 = document.querySelector('.img1');
-let img2 = document.querySelector('.img2');
-let img3 = document.querySelector('.img3');
-let img4 = document.querySelector('.img4');
-img1.onclick = function(){
-    mainimg.src ='images/image-product-1.jpg';
-}
-img2.onclick = function(){
-    mainimg.src ='images/image-product-2.jpg';
-}
-img3.onclick = function(){
-    mainimg.src ='images/image-product-3.jpg';
-}
-img4.onclick = function(){
-    mainimg.src ='images/image-product-4.jpg';
-}
-// slide for version mobile
+
+
+
+
+
+
+
+
+
+
 
 
 
