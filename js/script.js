3bb9f4111618273a851bffa0624c9727e1a2af5b
siham <ma.broukysiham@gmail.com>
2022-09-01 23:08:41 +0200

    update respensive design

--- js/script.js
+++ js/script.js
@@ -22,14 +22,7 @@ let addtocard = document.querySelector('.addtocard');
 //total price 
  
 
-// addtocard.onclick = function(){
- 
-//   if(nb !==0){
-//     ntf = nb;
-//     notification.innerHTML = ntf;
- 
-//     notification.style.display='block';  
-//   }
+
  
  
  
@@ -56,81 +49,6 @@ mins.onclick = function(){
 
 
 
-// btn.onclick = function()
-// {
-//     ///empty
-//     if(ntf === 0){
-        
-  
-//         cartcontainer.style.display='block'
-//         let cart1 = document.createElement("span");
-//         let cart1text = document.createTextNode('cart');
-//         cart1.className='cart1';
-//         cart1.appendChild(cart1text);
-
-//         let pempty = document.createElement("p");
-//         let pemptytext = document.createTextNode('your cart empty');
-//         pempty.className='empty';
-//         pempty.appendChild(pemptytext);
-//         pempty.style.display='block'
-
-//         cartcontainer.appendChild(cart1);
-//         cartcontainer.appendChild(pempty);
-       
-
-//     }else{
-
-//         cartcontainer.style.display='block'
-//         console.log('hi dev siham');
-
-//         let cart1 = document.createElement("span");
-//         let cart1text = document.createTextNode('cart');
-//         cart1.className='cart1';
-//         cart1.appendChild(cart1text);
-//             //notempty
-//                 let notempty = document.createElement("div");
-//                 notempty.className='notempty';
-//                 //imagele
-//                 let imag = document.createElement("div");
-//                 let imspro = document.createElement("img");
-//                 imspro.src='images/image-product-1.jpg';
-//                 imag.appendChild(imspro);
-//                 //pric ele
-//                 let yourpric = document.createElement("div"); 
-//                 yourpric.className=('yourpric')
-//                 let p1 = document.createElement("p");
-//                 let p1text =document.createTextNode('aaaaaaaaaaaaa');
-//                 p1.appendChild(p1text);
-    
-//                 let p2 = document.createElement("p");
-//                 let p2text =document.createTextNode(`${nbPrice} x ${ntf}`);
-//                 p2.appendChild(p2text);  
-    
-//                 yourpric.appendChild(p1);
-//                 yourpric.appendChild(p2);
-//                     //suupp ele
-//                     let supp= document.createElement("div");
-//                     let icondelet =document.createElement('i');
-//                     icondelet.classList='fa fa-trash';
-//                 supp.appendChild(icondelet);
-        
-//             notempty.appendChild(imag);
-//             notempty.appendChild(yourpric);
-//             notempty.appendChild(supp);
-    
-//             //buttun checkout
-//             let butcheckout =document.createElement('button');
-//             let  butcheckouttext =document.createTextNode('checkout');
-//             butcheckout.appendChild(butcheckouttext);
-//             butcheckout.className=("checkout");
-//             notempty.appendChild(butcheckout);
-    
-//         cartcontainer.appendChild(cart1);
-//         cartcontainer.appendChild(notempty);
-//     }
-
-   
-// };
 // lin lsit
 let menu = document.querySelector('.menu');
 let menu1 = document.querySelector('.menu1');
@@ -165,6 +83,7 @@ img3.onclick = function(){
 img4.onclick = function(){
     mainimg.src ='images/image-product-4.jpg';
 }
+
 // slide for version mobile
 
 
@@ -174,19 +93,7 @@ img4.onclick = function(){
 
 
 
-// addtocard.onclick = function(){
  
-//     if(nb !==0){
-//       ntf = nb;
-//       notification.innerHTML = ntf;
-   
-//       notification.style.display='block';  
-//     }
-   
-   
-   
-  
-//   }
 
 
   addtocard.onclick = function()
