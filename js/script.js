53c5e3074268812b9969e7268c6d5aa0121e5444
siham <ma.broukysiham@gmail.com>
2022-11-19 23:46:50 +0100

    app -ecom

--- js/script.js
+++ js/script.js
@@ -106,92 +106,92 @@ function ChangeSlide(sens) {
  
 // ******************************************************************************************
 
-  addtocard.onclick = function()
-{
+//   addtocard.onclick = function()
+// {
 
-    if(nb !==0){
-        ntf = nb;
-        notification.innerHTML = ntf;
+//     if(nb !==0){
+//         ntf = nb;
+//         notification.innerHTML = ntf;
      
-        notification.style.display='block';  
-      }
+//         notification.style.display='block';  
+//       }
      
-    ///empty
+//     ///empty
   
    
-        if(ntf === 0){
+//         if(ntf === 0){
 
-            cartcontainer.style.display='block'
-            let cart1 = document.createElement("span");
-            let cart1text = document.createTextNode('cart');
-            cart1.className='cart1';
-            cart1.appendChild(cart1text);
+//             cartcontainer.style.display='block'
+//             let cart1 = document.createElement("span");
+//             let cart1text = document.createTextNode('cart');
+//             cart1.className='cart1';
+//             cart1.appendChild(cart1text);
     
-            let pempty = document.createElement("p");
-            let pemptytext = document.createTextNode('your cart empty');
-            pempty.className='empty';
-            pempty.appendChild(pemptytext);
-            pempty.style.display='block'
+//             let pempty = document.createElement("p");
+//             let pemptytext = document.createTextNode('your cart empty');
+//             pempty.className='empty';
+//             pempty.appendChild(pemptytext);
+//             pempty.style.display='block'
     
-            cartcontainer.appendChild(cart1);
-            cartcontainer.appendChild(pempty);
+//             cartcontainer.appendChild(cart1);
+//             cartcontainer.appendChild(pempty);
            
     
-        }else{
+//         }else{
     
-            cartcontainer.style.display='block'
-            console.log('hi dev siham');
+//             cartcontainer.style.display='block'
+//             console.log('hi dev siham');
     
-            let cart1 = document.createElement("span");
-            let cart1text = document.createTextNode('cart');
-            cart1.className='cart1';
-            cart1.appendChild(cart1text);
-                //notempty
-                    let notempty = document.createElement("div");
-                    notempty.className='notempty';
-                    //imagele
-                    let imag = document.createElement("div");
-                    let imspro = document.createElement("img");
-                    imspro.src= mainimg;
-                    imag.appendChild(imspro);
-                    //pric ele
-                    let yourpric = document.createElement("div"); 
-                    yourpric.className=('yourpric')
-                    let p1 = document.createElement("p");
-                    let p1text =document.createTextNode('aaaaaaaaaaaaa');
-                    p1.appendChild(p1text);
+//             let cart1 = document.createElement("span");
+//             let cart1text = document.createTextNode('cart');
+//             cart1.className='cart1';
+//             cart1.appendChild(cart1text);
+//                 //notempty
+//                     let notempty = document.createElement("div");
+//                     notempty.className='notempty';
+//                     //imagele
+//                     let imag = document.createElement("div");
+//                     let imspro = document.createElement("img");
+//                     imspro.src= mainimg;
+//                     imag.appendChild(imspro);
+//                     //pric ele
+//                     let yourpric = document.createElement("div"); 
+//                     yourpric.className=('yourpric')
+//                     let p1 = document.createElement("p");
+//                     let p1text =document.createTextNode('aaaaaaaaaaaaa');
+//                     p1.appendChild(p1text);
         
-                    let p2 = document.createElement("p");
-                    let p2text =document.createTextNode(`${nbPrice} x ${ntf}`);
-                    p2.appendChild(p2text);  
+//                     let p2 = document.createElement("p");
+//                     let p2text =document.createTextNode(`${nbPrice} x ${ntf}`);
+//                     p2.appendChild(p2text);  
         
-                    yourpric.appendChild(p1);
-                    yourpric.appendChild(p2);
-                        //suupp ele
-                        let supp= document.createElement("div");
-                        let icondelet =document.createElement('i');
-                        icondelet.classList='fa fa-trash';
-                    supp.appendChild(icondelet);
+//                     yourpric.appendChild(p1);
+//                     yourpric.appendChild(p2);
+//                         //suupp ele
+//                         let supp= document.createElement("div");
+//                         let icondelet =document.createElement('i');
+//                         icondelet.classList='fa fa-trash';
+//                     supp.appendChild(icondelet);
             
-                notempty.appendChild(imag);
-                notempty.appendChild(yourpric);
-                notempty.appendChild(supp);
+//                 notempty.appendChild(imag);
+//                 notempty.appendChild(yourpric);
+//                 notempty.appendChild(supp);
         
-                //buttun checkout
-                let butcheckout =document.createElement('button');
-                let  butcheckouttext =document.createTextNode('checkout');
-                butcheckout.appendChild(butcheckouttext);
-                butcheckout.className=("checkout");
-                notempty.appendChild(butcheckout);
+//                 //buttun checkout
+//                 let butcheckout =document.createElement('button');
+//                 let  butcheckouttext =document.createTextNode('checkout');
+//                 butcheckout.appendChild(butcheckouttext);
+//                 butcheckout.className=("checkout");
+//                 notempty.appendChild(butcheckout);
         
-            cartcontainer.appendChild(cart1);
-            cartcontainer.appendChild(notempty);
-        }
+//             cartcontainer.appendChild(cart1);
+//             cartcontainer.appendChild(notempty);
+//         }
  
 
    
-}
-// ******************************************************************************************
+// }
+// // ******************************************************************************************
  
 
 
